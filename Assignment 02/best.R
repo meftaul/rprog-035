fa57752905f5b3db68838fefe26de646ae1c2316
meftaul <meftaulhaque@gmail.com>
2015-12-24 14:00:11 +0600

    Assignment part 1 complete

--- Assignment 02/best.R
+++ Assignment 02/best.R
@@ -1,19 +1,37 @@
 best <- function(state, outcome){
+    
+    # Check outcome
+    possible_outcomes <- c("heart attack", "heart failure", "pneumonia")
+    if(is.na(match(outcome, possible_outcomes))){
+        stop("invalid outcome")
+    }
+    
     # Reading data
-    # outcome <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors = FALSE)
+    all_data <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors = FALSE)
     
     # Extracting necessary columns
-    # my_data <- outcome[, c(2, 7, 11, 17, 23)]
+    my_data <- all_data[, c(2, 7, 11, 17, 23)]
     
     # Renaming my_data
-    # names(my_data) <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+    names(my_data) <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+    
+    ## Check state validation
+    all_states <- unique(my_data$state)
+    if(is.na(match(state, all_states))){
+       stop("invalid state")
+    }
     
     # Removing na values
-    # clean_data <- na.omit(my_data)
+    clean_data <- na.omit(my_data)
     
-    # Extract state and outcome data
-    # clean_data[which(clean_data$state == "TX"), "heart attack"]
+    # Extract state data
+    state_data <- clean_data[which(clean_data$state == state),]
     
-    # subset(clean_data$hospital, clean_data$`heart attack` == min(clean_data[which(clean_data$state == "MD"), "heart attack"]))
+    # Find minimum of specific outcome
+    minValue <- min(state_data[, outcome])
     
+    # Find the best hospital
+    result_data <- state_data[which(state_data[, outcome] == minValue),]
+    
+    result_data$hospital
 }
