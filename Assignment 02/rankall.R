39129e789446e3d76dc987025201854750da973d
meftaul <meftaulhaque@gmail.com>
2015-12-27 13:08:37 +0600

    Final submission :)

--- Assignment 02/rankall.R
+++ Assignment 02/rankall.R
@@ -17,25 +17,56 @@ rankall <- function(outcome, num = "best"){
     # Removing na values
     clean_data <- na.omit(my_data)
     
+    # sorting data ordered by state and then hospital
+    clean_data <- clean_data[with(clean_data, order(clean_data$state,clean_data[, outcome], clean_data$hospital)),]
+    
     # Split data based on state
     splited_state_data <- split(clean_data, clean_data$state)
     
-    # al <- splited_state_data[['AL']]
-    # al <- al[with(al, order(al[, 'heart attack'])),]
-    # al[20,]
     
-    result <- data.frame()
+#     wi <- splited_state_data[['WV']]
+#     wi <- wi[with(wi, order(wi[, 'pneumonia'])),]
+#     wi <- wi[nrow(wi), ]
+#     print(wi[, 'hospital'])
+    
+    # output columns
+    hospital_names <- c()
+    state_names <- unique(clean_data$state)
+    
     for (i in 1:length(splited_state_data)) {
+        
         tmp <- splited_state_data[[i]]
         tmp <- tmp[with(tmp, order(tmp[, outcome])),]
+        # state_names <- c(state_names, )
+        # print(nrow(tmp))
+        
         if (num == 'best') {
-            result <- rbind(result, tmp[1,])
+            
+            tmp <- tmp[1,]
+            hospital_names <- c(hospital_names, tmp[, 'hospital'])
+            # result <- rbind(result, tmp[1,])
+            
         }else if (num == 'worst') {
-            result <- rbind(result, tmp[length(tmp),])
+            
+            tmp <- tmp[nrow(tmp),]
+            hospital_names <- c(hospital_names, tmp[, 'hospital'])
+            # result <- rbind(result, tmp[nrow(tmp),])
+            
         }else{
-            result <- rbind(result, tmp[num, ])
+            
+            tmp <- tmp[num,]
+            hospital_names <- c(hospital_names, tmp[, 'hospital'])
+            # result <- rbind(result, tmp[num, ])
+            
         }
     }
+    
+    # Processing results as dataframe
+    result <- as.data.frame(cbind(hospital_names, state_names))
+    
+    # renaming rows and columns
+    rownames(result) <- state_names
+    colnames(result) <- c('hospital', 'state')
     
     result
     
